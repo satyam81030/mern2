82be8e3b4ca95d691f6b450972cd107cfc67d4cb
satyam@81030 <solankisatyam3105@gmail.com>
2023-10-16 17:38:24 +0530

    now the branch change

---
+++ hel/u.js
@@ -0,0 +1,744 @@
+// const hel = [
+//    {
+//     score: 41,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2023-03-01T07:32:13.000Z''
+//   },
+//    {
+//     score: 48,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2023-03-02T01:17:18.000Z''
+//   },
+//    {
+//     score: 64,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2022-10-18T01:57:36.000Z''
+//   },
+//    {
+//     score: 59,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2023-03-02T04:16:49.000Z''
+//   },
+//    {
+//     score: 64,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2022-10-18T01:57:48.000Z''
+//   },
+//    {
+//     score: 29,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2022-10-18T01:57:51.000Z''
+//   },
+//    {
+//     score: 83,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2022-10-12T10:11:56.000Z''
+//   },
+//    {
+//     score: 100,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2022-10-12T10:13:45.000Z''
+//   },
+//    {
+//     score: 100,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2022-10-18T01:58:08.000Z''
+//   },
+//    {
+//     score: 65,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2023-01-30T14:18:45.000Z''
+//   },
+//    {
+//     score: 0,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2022-10-18T01:58:03.000Z''
+//   },
+//    {
+//     score: 100,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2022-10-18T07:54:30.000Z''
+//   },
+//    {
+//     score: 0,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2022-11-16T04:13:31.000Z''
+//   },
+//    {
+//     score: 100,
+//     orgId: 'ORG00026',
+//     assessmentDate:'' 2022-11-02T19:06:47.000Z''
+//   },
+//    {
+//     score: 100,
+//     orgId: 'ORG00084',
+//     assessmentDate:'' 2022-10-12T10:14:25.000Z''
+//   },
+//    {
+//     score: 1,
+//     orgId: 'ORG00084',
+//     assessmentDate:'' 2022-11-01T09:02:03.000Z''
+//   },
+//    {
+//     score: 55,
+//     orgId: 'ORG00084',
+//     assessmentDate:'' 2023-03-01T07:32:14.000Z''
+//   },
+//    {
+//     score: 40,
+//     orgId: 'ORG00084',
+//     assessmentDate:'' 2022-11-04T03:20:08.000Z''
+//   },
+//    {
+//     score: 46,
+//     orgId: 'ORG00084',
+//     assessmentDate:'' 2022-11-04T03:20:38.000Z''
+//   },
+//    {
+//     score: 80,
+//     orgId: 'ORG00084',
+//     assessmentDate:'' 2022-11-12T08:34:54.000Z''
+//   },
+//    {
+//     score: 60,
+//     orgId: 'ORG00093',
+//     assessmentDate:'' 2023-01-17T22:16:37.000Z''
+//   },
+//    {
+//     score: 43,
+//     orgId: 'ORG00094',
+//     assessmentDate:'' 2023-03-01T07:32:15.000Z''
+//   },
+//    {
+//     score: 70,
+//     orgId: 'ORG00103',
+//     assessmentDate:'' 2022-10-06T11:36:13.000Z''
+//   },
+//    {
+//     score: 93,
+//     orgId: 'ORG00103',
+//     assessmentDate:'' 2022-10-06T11:56:54.000Z''
+//   },
+//    {
+//     score: 38,
+//     orgId: 'ORG00104',
+//     assessmentDate:'' 2023-03-01T07:32:13.000Z''
+//   },
+//    {
+//     score: 55,
+//     orgId: 'ORG00104',
+//     assessmentDate:'' 2023-03-02T01:17:18.000Z''
+//   },
+//    {
+//     score: 50,
+//     orgId: 'ORG00104',
+//     assessmentDate:'' 2023-03-02T04:16:49.000Z''
+//   },
+//    {
+//     score: 63,
+//     orgId: 'ORG00104',
+//     assessmentDate:'' 2022-10-03T14:08:48.000Z''
+//   },
+//    {
+//     score: 66,
+//     orgId: 'ORG00104',
+//     assessmentDate:'' 2022-10-04T13:41:21.000Z''
+//   },
+//    {
+//     score: 1,
+//     orgId: 'ORG00104',
+//     assessmentDate:'' 2022-10-18T07:06:12.000Z''
+//   },
+//    {
+//     score: 31,
+//     orgId: 'ORG00104',
+//     assessmentDate:'' 2022-10-17T08:05:45.000Z''
+//   },
+//    {
+//     score: 14,
+//     orgId: 'ORG00104',
+//     assessmentDate:'' 2022-10-17T08:05:21.000Z''
+//   },
+//    {
+//     score: 57,
+//     orgId: 'ORG00108',
+//     assessmentDate:'' 2022-10-14T07:39:40.000Z''
+//   },
+//    {
+//     score: 31,
+//     orgId: 'ORG00109',
+//     assessmentDate:'' 2023-03-01T07:32:14.000Z''
+//   },
+//    {
+//     score: 41,
+//     orgId: 'ORG00123',
+//     assessmentDate:'' 2023-03-01T07:32:14.000Z''
+//   },
+//    {
+//     score: 43,
+//     orgId: 'ORG00123',
+//     assessmentDate:'' 2023-03-02T01:17:18.000Z''
+//   },
+//    {
+//     score: 34,
+//     orgId: 'ORG00123',
+//     assessmentDate:'' 2023-03-02T04:16:49.000Z''
+//   },
+//    {
+//     score: 49,
+//     orgId: 'ORG00123',
+//     assessmentDate:'' 2022-10-17T14:20:25.000Z''
+//   },
+//    {
+//     score: 1,
+//     orgId: 'ORG00123',
+//     assessmentDate:'' 2022-10-17T14:22:01.000Z''
+//   },
+//    {
+//     score: 57,
+//     orgId: 'ORG00123',
+//     assessmentDate:'' 2022-10-17T14:22:18.000Z''
+//   },
+//    {
+//     score: 94,
+//     orgId: 'ORG00123',
+//     assessmentDate:'' 2022-10-17T14:22:28.000Z''
+//   },
+//    {
+//     score: 53,
+//     orgId: 'ORG00123',
+//     assessmentDate:'' 2022-10-17T14:22:43.000Z''
+//   },
+//    {
+//     score: 1,
+//     orgId: 'ORG00123',
+//     assessmentDate:'' 2022-10-17T14:22:53.000Z''
+//   },
+//    {
+//     score: 0,
+//     orgId: 'ORG00123',
+//     assessmentDate:'' 2022-10-17T14:22:56.000Z''
+//   },
+//    {
+//     score: 1,
+//     orgId: 'ORG00123',
+//     assessmentDate:'' 2022-10-18T07:06:12.000Z''
+//   },
+//    {
+//     score: 70,
+//     orgId: 'ORG00123',
+//     assessmentDate:'' 2023-01-30T14:18:45.000Z''
+//   },
+//    {
+//     score: 42,
+//     orgId: 'ORG00125',
+//     assessmentDate:'' 2023-03-01T07:32:14.000Z''
+//   },
+//    {
+//     score: 67,
+//     orgId: 'ORG00125',
+//     assessmentDate:'' 2022-10-17T14:28:45.000Z''
+//   },
+//    {
+//     score: 49,
+//     orgId: 'ORG00125',
+//     assessmentDate:'' 2023-03-02T01:17:19.000Z''
+//   },
+//    {
+//     score: 41,
+//     orgId: 'ORG00125',
+//     assessmentDate:'' 2023-03-02T04:16:49.000Z''
+//   },
+//    {
+//     score: 94,
+//     orgId: 'ORG00125',
+//     assessmentDate:'' 2023-01-20T05:14:07.000Z''
+//   },
+//    {
+//     score: 70,
+//     orgId: 'ORG00125',
+//     assessmentDate:'' 2023-01-30T14:18:45.000Z''
+//   },
+//    {
+//     score: 6,
+//     orgId: 'ORG00125',
+//     assessmentDate:'' 2023-02-15T15:09:04.000Z''
+//   },
+//    {
+//     score: 57,
+//     orgId: 'ORG00126',
+//     assessmentDate:'' 2023-03-01T07:32:14.000Z''
+//   },
+//    {
+//     score: 25,
+//     orgId: 'ORG00126',
+//     assessmentDate:'' 2023-03-02T01:17:19.000Z''
+//   },
+//    {
+//     score: 54,
+//     orgId: 'ORG00126',
+//     assessmentDate:'' 2023-03-02T04:16:49.000Z''
+//   },
+//    {
+//     score: 100,
+//     orgId: 'ORG00126',
+//     assessmentDate:'' 2022-11-14T16:26:06.000Z''
+//   },
+//    {
+//     score: 100,
+//     orgId: 'ORG00126',
+//     assessmentDate:'' 2022-11-14T16:26:35.000Z''
+//   },
+//    {
+//     score: 66,
+//     orgId: 'ORG00126',
+//     assessmentDate:'' 2023-01-24T21:17:34.000Z''
+//   },
+//    {
+//     score: 67,
+//     orgId: 'ORG00133',
+//     assessmentDate:'' 2022-10-19T10:56:29.000Z''
+//   },
+//    {
+//     score: 52,
+//     orgId: 'ORG00134',
+//     assessmentDate:'' 2023-03-01T07:32:14.000Z''
+//   },
+//    {
+//     score: 51,
+//     orgId: 'ORG00134',
+//     assessmentDate:'' 2023-03-02T01:17:18.000Z''
+//   },
+//    {
+//     score: 42,
+//     orgId: 'ORG00134',
+//     assessmentDate:'' 2023-03-02T04:16:49.000Z''
+//   },
+//    {
+//     score: 41,
+//     orgId: 'ORG00134',
+//     assessmentDate:'' 2023-01-30T08:00:42.000Z''
+//   },
+//    {
+//     score: 49,
+//     orgId: 'ORG00142',
+//     assessmentDate:'' 2023-03-01T07:32:15.000Z''
+//   },
+//    {
+//     score: 71,
+//     orgId: 'ORG00166',
+//     assessmentDate:'' 2022-12-07T04:06:57.000Z''
+//   },
+//    {
+//     score: 72,
+//     orgId: 'ORG00178',
+//     assessmentDate:'' 2023-01-04T04:30:11.000Z''
+//   },
+//    {
+//     score: 58,
+//     orgId: 'ORG00182',
+//     assessmentDate:'' 2023-01-05T08:43:56.000Z''
+//   },
+//    {
+//     score: 77,
+//     orgId: 'ORG00183',
+//     assessmentDate:'' 2023-01-05T09:00:43.000Z''
+//   },
+//    {
+//     score: 69,
+//     orgId: 'ORG00188',
+//     assessmentDate:'' 2023-01-20T06:24:21.000Z''
+//   },
+//    {
+//     score: 52,
+//     orgId: 'ORG00188',
+//     assessmentDate:'' 2023-03-01T07:32:15.000Z''
+//   },
+//    {
+//     score: 38,
+//     orgId: 'ORG00194',
+//     assessmentDate:'' 2023-03-01T07:32:15.000Z''
+//   }
+// ]
+const hel =  [
+   {
+    score: 41,
+    orgId: 'ORG00026',
+    assessmentDate:' 2023-03-01T07:32:13.000Z'
+  },
+   {
+    score: 48,
+    orgId: 'ORG00026',
+    assessmentDate:' 2023-03-02T01:17:18.000Z'
+  },
+   {
+    score: 64,
+    orgId: 'ORG00026',
+    assessmentDate:' 2022-10-18T01:57:36.000Z'
+  },
+   {
+    score: 59,
+    orgId: 'ORG00026',
+    assessmentDate:' 2023-03-02T04:16:49.000Z'
+  },
+   {
+    score: 64,
+    orgId: 'ORG00026',
+    assessmentDate:' 2022-10-18T01:57:48.000Z'
+  },
+   {
+    score: 29,
+    orgId: 'ORG00026',
+    assessmentDate:' 2022-10-18T01:57:51.000Z'
+  },
+   {
+    score: 83,
+    orgId: 'ORG00026',
+    assessmentDate:' 2022-10-12T10:11:56.000Z'
+  },
+   {
+    score: 100,
+    orgId: 'ORG00026',
+    assessmentDate:' 2022-10-12T10:13:45.000Z'
+  },
+   {
+    score: 100,
+    orgId: 'ORG00026',
+    assessmentDate:' 2022-10-18T01:58:08.000Z'
+  },
+   {
+    score: 65,
+    orgId: 'ORG00026',
+    assessmentDate:' 2023-01-30T14:18:45.000Z'
+  },
+   {
+    score: 0,
+    orgId: 'ORG00026',
+    assessmentDate:' 2022-10-18T01:58:03.000Z'
+  },
+   {
+    score: 100,
+    orgId: 'ORG00026',
+    assessmentDate:' 2022-10-18T07:54:30.000Z'
+  },
+   {
+    score: 0,
+    orgId: 'ORG00026',
+    assessmentDate:' 2022-11-16T04:13:31.000Z'
+  },
+   {
+    score: 100,
+    orgId: 'ORG00026',
+    assessmentDate:' 2022-11-02T19:06:47.000Z'
+  },
+   {
+    score: 100,
+    orgId: 'ORG00084',
+    assessmentDate:' 2022-10-12T10:14:25.000Z'
+  },
+   {
+    score: 1,
+    orgId: 'ORG00084',
+    assessmentDate:' 2022-11-01T09:02:03.000Z'
+  },
+   {
+    score: 55,
+    orgId: 'ORG00084',
+    assessmentDate:' 2023-03-01T07:32:14.000Z'
+  },
+   {
+    score: 40,
+    orgId: 'ORG00084',
+    assessmentDate:' 2022-11-04T03:20:08.000Z'
+  },
+   {
+    score: 46,
+    orgId: 'ORG00084',
+    assessmentDate:' 2022-11-04T03:20:38.000Z'
+  },
+   {
+    score: 80,
+    orgId: 'ORG00084',
+    assessmentDate:' 2022-11-12T08:34:54.000Z'
+  },
+   {
+    score: 60,
+    orgId: 'ORG00093',
+    assessmentDate:' 2023-01-17T22:16:37.000Z'
+  },
+   {
+    score: 43,
+    orgId: 'ORG00094',
+    assessmentDate:' 2023-03-01T07:32:15.000Z'
+  },
+   {
+    score: 70,
+    orgId: 'ORG00103',
+    assessmentDate:' 2022-10-06T11:36:13.000Z'
+  },
+   {
+    score: 93,
+    orgId: 'ORG00103',
+    assessmentDate:' 2022-10-06T11:56:54.000Z'
+  },
+   {
+    score: 38,
+    orgId: 'ORG00104',
+    assessmentDate:' 2023-03-01T07:32:13.000Z'
+  },
+   {
+    score: 55,
+    orgId: 'ORG00104',
+    assessmentDate:' 2023-03-02T01:17:18.000Z'
+  },
+   {
+    score: 50,
+    orgId: 'ORG00104',
+    assessmentDate:' 2023-03-02T04:16:49.000Z'
+  },
+   {
+    score: 63,
+    orgId: 'ORG00104',
+    assessmentDate:' 2022-10-03T14:08:48.000Z'
+  },
+   {
+    score: 66,
+    orgId: 'ORG00104',
+    assessmentDate:' 2022-10-04T13:41:21.000Z'
+  },
+   {
+    score: 1,
+    orgId: 'ORG00104',
+    assessmentDate:' 2022-10-18T07:06:12.000Z'
+  },
+   {
+    score: 31,
+    orgId: 'ORG00104',
+    assessmentDate:' 2022-10-17T08:05:45.000Z'
+  },
+   {
+    score: 14,
+    orgId: 'ORG00104',
+    assessmentDate:' 2022-10-17T08:05:21.000Z'
+  },
+   {
+    score: 57,
+    orgId: 'ORG00108',
+    assessmentDate:' 2022-10-14T07:39:40.000Z'
+  },
+   {
+    score: 31,
+    orgId: 'ORG00109',
+    assessmentDate:' 2023-03-01T07:32:14.000Z'
+  },
+   {
+    score: 41,
+    orgId: 'ORG00123',
+    assessmentDate:' 2023-03-01T07:32:14.000Z'
+  },
+   {
+    score: 43,
+    orgId: 'ORG00123',
+    assessmentDate:' 2023-03-02T01:17:18.000Z'
+  },
+   {
+    score: 34,
+    orgId: 'ORG00123',
+    assessmentDate:' 2023-03-02T04:16:49.000Z'
+  },
+   {
+    score: 49,
+    orgId: 'ORG00123',
+    assessmentDate:' 2022-10-17T14:20:25.000Z'
+  },
+   {
+    score: 1,
+    orgId: 'ORG00123',
+    assessmentDate:' 2022-10-17T14:22:01.000Z'
+  },
+   {
+    score: 57,
+    orgId: 'ORG00123',
+    assessmentDate:' 2022-10-17T14:22:18.000Z'
+  },
+   {
+    score: 94,
+    orgId: 'ORG00123',
+    assessmentDate:' 2022-10-17T14:22:28.000Z'
+  },
+   {
+    score: 53,
+    orgId: 'ORG00123',
+    assessmentDate:' 2022-10-17T14:22:43.000Z'
+  },
+   {
+    score: 1,
+    orgId: 'ORG00123',
+    assessmentDate:' 2022-10-17T14:22:53.000Z'
+  },
+   {
+    score: 0,
+    orgId: 'ORG00123',
+    assessmentDate:' 2022-10-17T14:22:56.000Z'
+  },
+   {
+    score: 1,
+    orgId: 'ORG00123',
+    assessmentDate:' 2022-10-18T07:06:12.000Z'
+  },
+   {
+    score: 70,
+    orgId: 'ORG00123',
+    assessmentDate:' 2023-01-30T14:18:45.000Z'
+  },
+   {
+    score: 42,
+    orgId: 'ORG00125',
+    assessmentDate:' 2023-03-01T07:32:14.000Z'
+  },
+   {
+    score: 67,
+    orgId: 'ORG00125',
+    assessmentDate:' 2022-10-17T14:28:45.000Z'
+  },
+   {
+    score: 49,
+    orgId: 'ORG00125',
+    assessmentDate:' 2023-03-02T01:17:19.000Z'
+  },
+   {
+    score: 41,
+    orgId: 'ORG00125',
+    assessmentDate:' 2023-03-02T04:16:49.000Z'
+  },
+   {
+    score: 94,
+    orgId: 'ORG00125',
+    assessmentDate:' 2023-01-20T05:14:07.000Z'
+  },
+   {
+    score: 70,
+    orgId: 'ORG00125',
+    assessmentDate:' 2023-01-30T14:18:45.000Z'
+  },
+   {
+    score: 6,
+    orgId: 'ORG00125',
+    assessmentDate:' 2023-02-15T15:09:04.000Z'
+  },
+   {
+    score: 57,
+    orgId: 'ORG00126',
+    assessmentDate:' 2023-03-01T07:32:14.000Z'
+  },
+   {
+    score: 25,
+    orgId: 'ORG00126',
+    assessmentDate:' 2023-03-02T01:17:19.000Z'
+  },
+   {
+    score: 54,
+    orgId: 'ORG00126',
+    assessmentDate:' 2023-03-02T04:16:49.000Z'
+  },
+   {
+    score: 100,
+    orgId: 'ORG00126',
+    assessmentDate:' 2022-11-14T16:26:06.000Z'
+  },
+   {
+    score: 100,
+    orgId: 'ORG00126',
+    assessmentDate:' 2022-11-14T16:26:35.000Z'
+  },
+   {
+    score: 66,
+    orgId: 'ORG00126',
+    assessmentDate:' 2023-01-24T21:17:34.000Z'
+  },
+   {
+    score: 67,
+    orgId: 'ORG00133',
+    assessmentDate:' 2022-10-19T10:56:29.000Z'
+  },
+   {
+    score: 52,
+    orgId: 'ORG00134',
+    assessmentDate:' 2023-03-01T07:32:14.000Z'
+  },
+   {
+    score: 51,
+    orgId: 'ORG00134',
+    assessmentDate:' 2023-03-02T01:17:18.000Z'
+  },
+   {
+    score: 42,
+    orgId: 'ORG00134',
+    assessmentDate:' 2023-03-02T04:16:49.000Z'
+  },
+   {
+    score: 41,
+    orgId: 'ORG00134',
+    assessmentDate:' 2023-01-30T08:00:42.000Z'
+  },
+   {
+    score: 49,
+    orgId: 'ORG00142',
+    assessmentDate:' 2023-03-01T07:32:15.000Z'
+  },
+   {
+    score: 71,
+    orgId: 'ORG00166',
+    assessmentDate:' 2022-12-07T04:06:57.000Z'
+  },
+   {
+    score: 72,
+    orgId: 'ORG00178',
+    assessmentDate:' 2023-01-04T04:30:11.000Z'
+  },
+   {
+    score: 58,
+    orgId: 'ORG00182',
+    assessmentDate:' 2023-01-05T08:43:56.000Z'
+  },
+   {
+    score: 77,
+    orgId: 'ORG00183',
+    assessmentDate:' 2023-01-05T09:00:43.000Z'
+  },
+   {
+    score: 69,
+    orgId: 'ORG00188',
+    assessmentDate:' 2023-01-20T06:24:21.000Z'
+  },
+   {
+    score: 52,
+    orgId: 'ORG00188',
+    assessmentDate:' 2023-03-01T07:32:15.000Z'
+  },
+   {
+    score: 38,
+    orgId: 'ORG00194',
+    assessmentDate:' 2023-03-01T07:32:15.000Z'
+  }
+] 
+
+  let length = hel.length-1
+  console.log(length);
+  let count = 0;
+  let counts=0;
+  let max=-1;
+ while(length>=0){
+    // console.log(hel[length].score<=66 &&  hel[length].score>=33)
+    if (hel[length].score<=66 &&  hel[length].score>=33) {
+      count++;
+    }
+    // if (hel[length]=66 &&  hel[length]>=33) {
+    //   count++;
+    // }
+    else{
+        counts++;
+    }
+    length--;
+ }
+ console.log("u",count, counts);
